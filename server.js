a7de7b4749974af3e2628800359d66d00f494fe9
yordanis <yordanis@gmail.com>
2021-07-22 15:57:52 -0400

    Habilitado por el puerto 8080

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
   bGround.log('Node is listening on port '+ port + '...')
 });
