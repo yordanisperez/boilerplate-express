8eb814ae96971bc6b6715fe0fdc594548ff9d121
yordanis <yordanis@gmail.com>
2021-07-25 12:01:09 -0400

    Challenge #12

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',(req,res)=>{
-    res.send("<h1>Hola Adrian. Nuestro servidor de pruebas con <spam>https</spam></h1>");
+    res.sendFile(__dirname+"/views/index.html");
 })
 
 console.log("Hello World");
