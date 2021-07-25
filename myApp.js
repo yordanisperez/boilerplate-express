c61e6559bf96d11894ce6cdd0af6582e7918d1c2
yordanis <yordanis@gmail.com>
2021-07-25 12:16:46 -0400

    Challenge #12 with styles

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use('/public',express.static(__dirname+"/public"));
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/views/index.html");
 })
