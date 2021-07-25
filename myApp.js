aa1f0c739812373f22a76177f22ab0e713f4b409
yordanis <yordanis@gmail.com>
2021-07-25 13:10:55 -0400

    Add  environment variables

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
+
 var express = require('express');
 var app = express();
 
 app.use('/public',express.static(__dirname+"/public"));
 
 app.get('/json',(req,res)=>{
-    res.send({"message": "Hello json"});
+    console.log(process.env.MESSAGE_STYLE);
+    if (process.env.MESSAGE_STYLE==='uppercase')
+        res.send({"message": "HELLO JSON"});
+    else
+        res.send({"message": "Hello json"});
 })
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/views/index.html");
