427f90bb73a0a52d40bb026dd424822a6ae4582d
yordanis <yordanis@gmail.com>
2021-07-25 12:27:04 -0400

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.use('/public',express.static(__dirname+"/public"));
 
+app.get('/json',(req,res)=>{
+    res.send({"message": "Hello json"});
+})
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/views/index.html");
 })
