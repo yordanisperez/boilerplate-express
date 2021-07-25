b416bebd8a194a314b960516a83224d307e7918a
yordanis <yordanis@gmail.com>
2021-07-25 17:52:01 -0400

    Not run testing freecodecamp

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
-    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.org'];
+    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
     var origin = req.headers.origin || '*';
     if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
 
